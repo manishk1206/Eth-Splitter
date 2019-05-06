b2c250d2fb5bfd5d82faaba5d070b5fa146df7ba
manishk1206 <44619042+manishk1206@users.noreply.github.com>
2019-05-06 22:14:45 +0530

    Update 2_deploy_contracts.js

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,8 +1,5 @@
-const ConvertLib = artifacts.require("ConvertLib");
-const MetaCoin = artifacts.require("MetaCoin");
+const ConvertLib = artifacts.require("Splitter");
 
 module.exports = function(deployer) {
-  deployer.deploy(ConvertLib);
-  deployer.link(ConvertLib, MetaCoin);
-  deployer.deploy(MetaCoin);
+  deployer.deploy(Splitter);
 };
