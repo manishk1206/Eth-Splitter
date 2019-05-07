5e415ad1f06311907d735a5bd4003294935c8b08
manishk1206 <44619042+manishk1206@users.noreply.github.com>
2019-05-08 02:11:42 +0530

    Made the contract as a utility
    Incorporated : other changes + Point 1(make splitter generic) 
    Pending: point 2 and point 3 )ownable and pausable)

--- contracts/Splitter.sol
+++ contracts/Splitter.sol
@@ -1,44 +1,44 @@
 pragma solidity ^0.5.0;
 
+import "./SafeMath.sol";
+
 contract Splitter{
     
-    address public Alice;
+    using SafeMath for uint;
+    
     mapping(address => uint) public balances;
     
-    event LogSplitFunds(address indexed beneficiary, uint indexed amount, uint indexed mywei);
+    event LogSplitFunds(address indexed splitby,address indexed beneiciary, uint indexed amount);
     event LogWithdrawnFunds(address indexed beneficiary, uint indexed amount);
 
     constructor () public payable{
-        Alice = msg.sender;
-    }
-    
-    modifier onlyAlice(){
         
-        require(msg.sender == Alice, "You are not Alice almighty, buzz off!" );
-        _;
     }
     
-    //Main function that is used to Split the funds by Alice
-    function splitBalance(address _bob, address _carol) public payable onlyAlice returns(bool isSuccess){
+    //Main function that is used to Split the funds
+    function splitBalance(address _bob, address _carol) public payable returns(bool isSuccess){
 
         require ( msg.value > 0, "There should be some non-zero value to split" );
         
         // Bob and Carol should have a valid address
-        require( _bob != address(0)&&_carol != address(0));
-
+        require( _bob != address(0) && _carol != address(0));
+        
+        //All three accounts should be different to make sense
+        require( _bob != msg.sender && _carol != msg.sender && _bob != _carol,"Duplicate address");
+        
         uint split = msg.value/2;
         uint mywei = msg.value%2;  //in case of odd msg.value, 1 wei will be left
         
-        balances[_bob] += split;
-        emit LogSplitFunds(_bob,balances[_bob],0);
+        balances[_bob] = balances[_bob].add(split);
+        emit LogSplitFunds(msg.sender,_bob,balances[_bob]);
         
-        balances[_carol] += split;
-        emit LogSplitFunds(_carol,balances[_carol],0);
+        balances[_carol] = balances[_carol].add(split);
+        emit LogSplitFunds(msg.sender,_carol,balances[_carol]);
         
         if (mywei > 0) {
-            address(msg.sender).transfer(mywei);
+            balances[msg.sender] = balances[msg.sender].add(mywei);
         }
-        emit LogSplitFunds(msg.sender,0,mywei);
+        emit LogSplitFunds(msg.sender,msg.sender,mywei);
         
         return true;
     }
@@ -48,16 +48,11 @@ contract Splitter{
         uint myBalance = balances[msg.sender];
         require(myBalance > 0, "No balance to withdraw");
         balances[msg.sender] = 0; // Making balance 0 in the mapping as it would be withdrawn 
-        address(msg.sender).transfer(myBalance);
+        msg.sender.transfer(myBalance);
         emit LogWithdrawnFunds(msg.sender, myBalance);
     }
     
-    //Kill the contract and transfer any remaining funds back to owner(Alice)
-    function kill() external onlyAlice {
-        selfdestruct(msg.sender); 
-    }
-    
-    //fall-back function
+    //fall-back function, not payable
     function() external {
         revert("Default Call to fallback");
     }
