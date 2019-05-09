9f763ce6fa4a162a047856e2e3d04bb1a535114b
manishk1206 <44619042+manishk1206@users.noreply.github.com>
2019-05-09 11:44:58 +0530

    Inherited Ownable, Pausable plus cosmetic changes

--- contracts/Splitter.sol
+++ contracts/Splitter.sol
@@ -1,8 +1,9 @@
 pragma solidity ^0.5.0;
 
 import "./SafeMath.sol";
+import "./Pausable.sol";
 
-contract Splitter{
+contract Splitter is Pausable{
     
     using SafeMath for uint;
     
@@ -16,42 +17,41 @@ contract Splitter{
     }
     
     //Main function that is used to Split the funds
-    function splitBalance(address _bob, address _carol) public payable returns(bool isSuccess){
+    function splitBalance(address receiver1, address receiver2) public payable onlyOwner onlyIfRunning returns(bool isSuccess) {
 
         require ( msg.value > 0, "There should be some non-zero value to split" );
         
         // Bob and Carol should have a valid address
-        require( _bob != address(0) && _carol != address(0));
+        require( receiver1 != address(0) && receiver2 != address(0));
         
         //All three accounts should be different to make sense
-        require( _bob != msg.sender && _carol != msg.sender && _bob != _carol,"Duplicate address");
+        require( receiver1 != msg.sender && receiver2 != msg.sender && receiver1 != receiver2,"Duplicate address");
         
         uint split = msg.value/2;
         uint mywei = msg.value%2;  //in case of odd msg.value, 1 wei will be left
         
-        balances[_bob] = balances[_bob].add(split);
-        emit LogSplitFunds(msg.sender,_bob,balances[_bob]);
+        balances[receiver1] = balances[receiver1].add(split);
+        emit LogSplitFunds(msg.sender,receiver1,split);
         
-        balances[_carol] = balances[_carol].add(split);
-        emit LogSplitFunds(msg.sender,_carol,balances[_carol]);
+        balances[receiver2] = balances[receiver2].add(split);
+        emit LogSplitFunds(msg.sender,receiver2,split);
         
         if (mywei > 0) {
             balances[msg.sender] = balances[msg.sender].add(mywei);
+            emit LogSplitFunds(msg.sender,msg.sender,mywei);
         }
-        emit LogSplitFunds(msg.sender,msg.sender,mywei);
-        
         return true;
     }
     
     //Pull pattern for withdrawing funds as suggested by Adel
-    function withdrawFunds() public  {
+    function withdrawFunds() public onlyIfRunning {
         uint myBalance = balances[msg.sender];
         require(myBalance > 0, "No balance to withdraw");
         balances[msg.sender] = 0; // Making balance 0 in the mapping as it would be withdrawn 
         msg.sender.transfer(myBalance);
         emit LogWithdrawnFunds(msg.sender, myBalance);
     }
-    
+        
     //fall-back function, not payable
     function() external {
         revert("Default Call to fallback");
