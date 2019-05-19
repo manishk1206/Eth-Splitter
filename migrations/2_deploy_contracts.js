9337d5465bf775f64de595e52ec1bd4dc74b7ffe
Manish Kumar <44619042+manishk1206@users.noreply.github.com>
2019-05-19 11:17:24 +0530

    Update 2_deploy_contracts.js

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,5 @@
 const Splitter = artifacts.require("Splitter");
 
 module.exports = function(deployer) {
-  deployer.deploy(Splitter);
+  deployer.deploy(Splitter,true); //true for bool _state of Pausable
 };
